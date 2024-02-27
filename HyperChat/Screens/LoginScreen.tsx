d1bdfd879a0397329b9c8203b9d417c648b19383
Davi <hoanghienle69@gmail.com>
2024-02-27 17:08:27 +0700

    update login screen

--- HyperChat/Screens/LoginScreen.tsx
+++ HyperChat/Screens/LoginScreen.tsx
@@ -1,6 +1,12 @@
 import React, {useState} from 'react';
-import {View, Text, TextInput, Pressable, StyleSheet} from 'react-native';
-import Svg, {Circle} from 'react-native-svg';
+import {
+  View,
+  Text,
+  TextInput,
+  Pressable,
+  StyleSheet,
+  Image,
+} from 'react-native';
 
 export default function LoginScreen({navigation}: {navigation: any}) {
   const [username, setUsername] = useState('');
@@ -19,7 +25,7 @@ export default function LoginScreen({navigation}: {navigation: any}) {
       alignItems: 'center',
     },
     welcome: {
-      fontSize: 20,
+      fontSize: 72,
       fontWeight: 'bold',
       marginBottom: 10,
     },
@@ -32,47 +38,99 @@ export default function LoginScreen({navigation}: {navigation: any}) {
       paddingHorizontal: 10,
     },
     button: {
+      width: '80%',
+      height: 50,
       backgroundColor: 'blue',
       padding: 10,
       borderRadius: 5,
+      justifyContent: 'center',
+      alignItems: 'center',
     },
     buttonText: {
       color: 'white',
-      fontSize: 16,
+      fontSize: 20,
       fontWeight: 'bold',
+    },
+    circle: {
+      width: 60,
+      height: 60,
+      borderRadius: 30,
+      borderWidth: 1,
+      marginHorizontal: 10,
+      alignItems: 'center',
+      justifyContent: 'center',
     },
   });
 
   return (
     <View style={styles.container}>
-      <Text style={styles.welcome}>Welcome to Login Screen!</Text>
-      <TextInput
-        style={styles.input}
-        placeholder="Username"
-        value={username}
-        onChangeText={setUsername}
+      <Image
+        style={{width: 300, height: 100, resizeMode: 'contain'}}
+        source={require('../Images/logo.png')}
       />
-      <TextInput
-        style={styles.input}
-        placeholder="Password"
-        secureTextEntry
-        value={password}
-        onChangeText={setPassword}
-      />
-      <Pressable onPress={handleLogin} style={styles.button}>
-        <Text style={styles.buttonText}>Login</Text>
-      </Pressable>
 
-      <Svg height="100" width="100">
-        <Circle
-          cx="50"
-          cy="50"
-          r="40"
-          stroke="black"
-          strokeWidth="3"
-          fill="red"
-        />
-      </Svg>
+      <View
+        style={{
+          width: '100%',
+          paddingLeft: 50,
+          alignItems: 'flex-start',
+          marginBottom: 10,
+        }}>
+        <Text style={styles.welcome}>Connect</Text>
+        <Text style={styles.welcome}>friends</Text>
+        <Text style={styles.welcome}>easily &</Text>
+        <Text style={styles.welcome}>quickly</Text>
+      </View>
+
+      <View
+        style={{
+          width: '100%',
+          paddingLeft: 50,
+          alignItems: 'flex-start',
+          marginBottom: 10,
+        }}>
+        <Text style={{fontSize: 24}}>Our chat app is the perfect way to</Text>
+        <Text style={{fontSize: 24}}>stay connected with friends and</Text>
+        <Text style={{fontSize: 24}}>family.</Text>
+      </View>
+
+      <View style={{flexDirection: 'row', marginVertical: 10}}>
+        <Pressable style={styles.circle}>
+          <Image
+            style={{width: 30, height: 30, resizeMode: 'contain'}}
+            source={require('../Images/Icon/facebook.png')}
+          />
+        </Pressable>
+        <Pressable style={styles.circle}>
+          <Image
+            style={{width: 30, height: 30, resizeMode: 'contain'}}
+            source={require('../Images/Icon/google.png')}
+          />
+        </Pressable>
+        <Pressable style={styles.circle}>
+          <Image
+            style={{width: 30, height: 30, resizeMode: 'contain'}}
+            source={require('../Images/Icon/apple.png')}
+          />
+        </Pressable>
+      </View>
+
+      <View
+        style={{
+          flexDirection: 'row',
+          alignItems: 'center',
+          marginVertical: 10,
+        }}>
+        <View style={{width: '35%', height: 1, borderBottomWidth: 1}} />
+        <Text style={{fontSize: 16, textAlign: 'center', marginHorizontal: 10}}>
+          OR
+        </Text>
+        <View style={{width: '35%', height: 1, borderBottomWidth: 1}} />
+      </View>
+
+      <Pressable onPress={handleLogin} style={styles.button}>
+        <Text style={styles.buttonText}>Sign up with mail</Text>
+      </Pressable>
     </View>
   );
 }
